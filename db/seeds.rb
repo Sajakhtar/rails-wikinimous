78db4f1e74b85a960947d7abe4972c09421f58d6
Sajid Akhtar <sajid.i.akhtar@gmail.com>
2021-08-09 16:10:41 +0400

    add article routes, controller, view, bootstrap stylying with simple form, font awesome icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Movie.title,
+    content: Faker::Movie.quote
+  )
+  article.save!
+end
+puts 'Finished!'
